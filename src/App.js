bbc678e16e018b4917b92a595617d08cf0f8e6cc
anshasiq <ahmadanshasi123@hotmail.com>
2023-07-12 19:36:38 +0000

    class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from "react";
+import { Routes, Route } from 'react-router-dom';
+
 import "./App.css";
 // import footer from "./components/footer/footer";
 import Home from "./components/home/Home";
+import Tours from "./components/tours/tours";
 // import 
 // import tours from "./components/tours/tours";
 // import header from "./components/header/header";
 function App() {
   return (
     <div>
-   
-   <Home />
-  
+   <Routes>
+ <Route path='/' element={<Home />} />
+ <Route path='/sec' element={<Tours />} />
+</Routes>
+<Home />
    </div>
   );
 }
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+function Nav() {
+    return (
+        <nav>
+            
+            <Link to='/first'>First</Link>
+            <Link to='/sec'>tours</Link>
+
+        </nav>
+    );
+}
+export default Nav;
