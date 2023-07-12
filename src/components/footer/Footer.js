2a1bfe1d372c399af4ed70f2069cce3c03a0f3aa
anshasiq <ahmadanshasi123@hotmail.com>
2023-07-12 11:14:24 +0000

    class

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./footer.css"
+function Footer() {
+    return (
+        <div>
+          <h4>social media links : <a href="https://web.facebook.com/" >facebook</a></h4>
+          <h4>Author  : Ahmad Anshasi  </h4>
+        </div>
+
+
+    );
+}
+export default Footer;
